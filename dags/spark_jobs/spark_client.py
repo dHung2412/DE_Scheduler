970a356eda69f4abed1f54dc60506217b2b66407
dHung2412 <danghung241204@gmail.com>
2025-11-29 00:39:48 +0700

    #6 [Update] Spark Client

--- dags/spark_jobs/spark_client.py
+++ dags/spark_jobs/spark_client.py
@@ -1,8 +1,12 @@
 import os
 import sys
 import logging
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+
 from pyspark.sql import SparkSession
-from dags.spark_jobs.config_2 import Config_2
+from config_2 import Config_2
 
 config = Config_2()
 
@@ -11,58 +15,28 @@ class SparkClient:
         """
         app_name
         job_type: 'streaming' -> Tốn tài nguyên
-                  'bathc' -> Tiết kiệm tài nguyên
+                  'batch' -> Tiết kiệm tài nguyên
         """
         self.app_name = app_name
         self.job_type = job_type
         self.logger = logging.getLogger(__name__)
 
+        self.logger.info(f"-----> [DEBUG] Python Executable: {sys.executable}")
+
         os.environ['PYSPARK_PYTHON'] = sys.executable
         os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-
-        self.spark_home = os.environ.get('SPARK_HOME')
-        self.jars_dir = os.path.join(self.spark_home, "jars")
-
-        self.required_jars = [
-            "hadoop-aws-3.3.4.jar",
-            "iceberg-spark-runtime-3.3_2.12-1.4.3.jar",
-            "iceberg-aws-bundle-1.4.3.jar",
-            "aws-java-sdk-bundle-1.12.262.jar",
-            "spark-sql-kafka-0-10_2.12-3.3.4.jar",
-            "kafka-clients-2.8.1.jar",
-            "commons-pool2-2.11.1.jar",
-            "spark-token-provider-kafka-0-10_2.12-3.3.4.jar"
-        ]
-
-    def _get_jars_conf(self):
-
-        valid_jars = []
-
-        for jar_name in self.required_jars:
-            jar_path = os.path.join(self.jars_dir, jar_name)
-
-            if os.path.exists(jar_path):
-                valid_jars.append(jar_path)
-
-            else:
-                self.logger.warning(f"-----> [WARNING] Thiếu JAR: {jar_path}")
-
-        return ",".join(valid_jars)
     
     def get_session(self):
         
         self.logger.info(f"-----> [SPARK_CLIENT] [{self.app_name}] Khởi tạo Spark Session ({self.job_type})...")    
-
-        jars_conf = self._get_jars_conf()
 
         master_conf = "local[*]" if self.job_type == "streaming" else "local[2]"
 
         builder= SparkSession.builder \
             .appName(self.app_name) \
             .master(master_conf) \
-            .config("spark.jars", jars_conf) \
             \
-            .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
+            .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
             .config("spark.hadoop.fs.s3a.access.key", config.MINIO_ROOT_USER) \
             .config("spark.hadoop.fs.s3a.secret.key", config.MINIO_ROOT_PASSWORD) \
             .config("spark.hadoop.fs.s3a.path.style.access", "true") \
@@ -74,10 +48,10 @@ class SparkClient:
             .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
             .config("spark.sql.catalog.demo", "org.apache.iceberg.spark.SparkCatalog") \
             .config("spark.sql.catalog.demo.type", "rest") \
-            .config("spark.sql.catalog.demo.uri", "http://localhost:8181") \
+            .config("spark.sql.catalog.demo.uri", "http://rest:8181") \
             .config("spark.sql.catalog.demo.warehouse", config.ICEBERG_WAREHOUSE_PATH) \
             .config("spark.sql.catalog.demo.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
-            .config("spark.sql.catalog.demo.s3.endpoint", "http://localhost:9000") \
+            .config("spark.sql.catalog.demo.s3.endpoint", "http://minio:9000") \
             .config("spark.sql.catalog.demo.s3.path-style-access", "true") \
             .config("spark.sql.catalog.demo.s3.connection.ssl.enabled", "false") \
             .config("spark.sql.catalog.demo.s3.access-key-id", config.MINIO_ROOT_USER) \
