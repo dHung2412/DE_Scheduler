8fe55ddd5c10a941764da2c6089ffc30f53f5d18
dHung2412 <danghung241204@gmail.com>
2025-12-06 21:20:04 +0700

    #17 [Fix] Remove comment

--- dags/spark_jobs/utils/sql/silver_table.sql
+++ dags/spark_jobs/utils/sql/silver_table.sql
@@ -17,23 +17,23 @@ CREATE TABLE IF NOT EXISTS {full_table_name} (
     payload_ref STRING,
     payload_ref_type STRING,
 
-    push_size INT,            -- Số lượng commit trong lần push này
-    push_distinct_size INT,   -- Số commit thực tế (không phải merge commit)
-    push_head_sha STRING,     -- Hash mới nhất
+    push_size INT,
+    push_distinct_size INT,
+    push_head_sha STRING,
 
     pr_number INT,
     pr_id BIGINT,
-    pr_state STRING,          -- open/closed
+    pr_state STRING,
     pr_title STRING,
-    pr_merged BOOLEAN,        -- True = Code đã được chấp nhận (Quan trọng)
-    pr_merged_at TIMESTAMP,   -- Thời điểm được chấp nhận
+    pr_merged BOOLEAN,
+    pr_merged_at TIMESTAMP,
 
     issue_number INT,
     issue_title STRING,
     issue_state STRING,
 
-    ingestion_date DATE,      -- Partition Key
-    processed_at TIMESTAMP    -- Thời điểm chạy job ETL
+    ingestion_date DATE,
+    processed_at TIMESTAMP
 )
 USING iceberg
 PARTITIONED BY (ingestion_date, event_type)
